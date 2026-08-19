6eb30297653730f33b1a29a3b11d9eab028cccca
Emmanuel66h <xybercoin.org.com@gmail.com>
2026-08-20 02:04:39 +0300

    Rebrand OBS Studio to Lightspeed Studio

--- frontend/widgets/OBSBasic_SysTray.cpp
+++ frontend/widgets/OBSBasic_SysTray.cpp
@@ -24,13 +24,13 @@ extern bool opt_minimize_tray;
 void OBSBasic::SystemTrayInit()
 {
 #ifdef __APPLE__
-	QIcon trayIconFile = QIcon(":/res/images/obs_macos.svg");
+	QIcon trayIconFile = QIcon(":/res/images/lightspeed_macos.svg");
 	trayIconFile.setIsMask(true);
 #else
-	QIcon trayIconFile = QIcon(":/res/images/obs.png");
+	QIcon trayIconFile = QIcon(":/res/images/lightspeed.png");
 #endif
-	trayIcon = new QSystemTrayIcon(QIcon::fromTheme("obs-tray", trayIconFile), this);
-	trayIcon->setToolTip("OBS Studio");
+	trayIcon = new QSystemTrayIcon(QIcon::fromTheme("lightspeed-tray", trayIconFile), this);
+	trayIcon->setToolTip("Lightspeed Studio");
 
 	trayMenu = new QMenu(this);
 
@@ -102,7 +102,7 @@ void OBSBasic::SysTrayNotify(const QString &text, QSystemTrayIcon::MessageIcon n
 {
 	if (trayIcon && trayIcon->isVisible() && QSystemTrayIcon::supportsMessages()) {
 		QSystemTrayIcon::MessageIcon icon = QSystemTrayIcon::MessageIcon(n);
-		trayIcon->showMessage("OBS Studio", text, icon, 10000);
+		trayIcon->showMessage("Lightspeed Studio", text, icon, 10000);
 	}
 }
 
